35c3f4dfce2335af2cdc88718ef683f6acb38c5d
Andreas Sieber <andreas.sieber@itsieber.ch>
2026-02-05 18:26:25 +0100

    Update Application.php

--- lib/AppInfo/Application.php
+++ lib/AppInfo/Application.php
@@ -18,6 +18,13 @@ use OCP\AppFramework\App;
 use OCP\AppFramework\Bootstrap\IBootContext;
 use OCP\AppFramework\Bootstrap\IBootstrap;
 use OCP\AppFramework\Bootstrap\IRegistrationContext;
+use OCP\IDBConnection;
+use OCP\Security\ICrypto;
+use Psr\Container\ContainerInterface;
+
+use OCA\OcSms\Db\ConfigMapper;
+use OCA\OcSms\Db\ConversationStateMapper;
+use OCA\OcSms\Db\SmsMapper;
 
 class Application extends App implements IBootstrap {
 	public const APP_ID = 'ocsms';
@@ -27,7 +34,31 @@ class Application extends App implements IBootstrap {
 	}
 
 	public function register(IRegistrationContext $context): void {
-		// Services are auto-wired via constructor injection
+		$context->registerService(ConversationStateMapper::class, function (ContainerInterface $c) {
+			return new ConversationStateMapper(
+				$c->get(IDBConnection::class)
+			);
+		});
+
+		$context->registerService(SmsMapper::class, function (ContainerInterface $c) {
+			return new SmsMapper(
+				$c->get(IDBConnection::class),
+				$c->get(ConversationStateMapper::class)
+			);
+		});
+
+		$context->registerService(ConfigMapper::class, function (ContainerInterface $c) {
+			$user = null;
+			$userSession = $c->get(\OCP\IUserSession::class);
+			if ($userSession->getUser() !== null) {
+				$user = $userSession->getUser()->getUID();
+			}
+			return new ConfigMapper(
+				$c->get(IDBConnection::class),
+				$user,
+				$c->get(ICrypto::class)
+			);
+		});
 	}
 
 	public function boot(IBootContext $context): void {
