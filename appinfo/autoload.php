07b85b7736b1da98ed6b079a5441d738fb0819f7
Andreas Sieber <andreas.sieber@itsieber.ch>
2026-02-05 18:31:21 +0100

    Create autoload.php

---
+++ appinfo/autoload.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+// Register autoloader for OCA\OcSms namespace
+spl_autoload_register(function ($className) {
+    $prefix = 'OCA\\OcSms\\';
+    $baseDir = __DIR__ . '/../lib/';
+
+    $len = strlen($prefix);
+    if (strncmp($prefix, $className, $len) !== 0) {
+        return;
+    }
+
+    $relativeClass = substr($className, $len);
+    $file = $baseDir . str_replace('\\', '/', $relativeClass) . '.php';
+
+    if (file_exists($file)) {
+        require $file;
+    }
+});
